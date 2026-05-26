6aff253e2783e8d9a8436d87cf83d7448a989a0d
ShohjahonAhmad <shox_0502@mail.ru>
2026-05-26 17:00:28 +0200

    add search by name or id

--- src/main/java/com/example/demo/controller/LiveRatingController.java
+++ src/main/java/com/example/demo/controller/LiveRatingController.java
@@ -12,7 +12,7 @@ import java.util.Objects;
 
 @RestController
 public class LiveRatingController {
-
+    private static final String DEFAULT_SEARCH = "";
     private final LiveRatingCacheService liveRatingCacheService;
 
 
@@ -22,9 +22,9 @@ public class LiveRatingController {
 
     @GetMapping("/top-ratings")
     public ResponseEntity<TopRatingsResponseDTO> getTopRatings() {
-        TopRatingResponseDTO stdRatings = liveRatingCacheService.findStdRatings(0, 100);
-        TopRatingResponseDTO rapidRatings = liveRatingCacheService.findRapidRatings(0, 100);
-        TopRatingResponseDTO blitzRatings = liveRatingCacheService.findBlitzRatings(0, 100);
+        TopRatingResponseDTO stdRatings = liveRatingCacheService.findStdRatings(0, 100, DEFAULT_SEARCH);
+        TopRatingResponseDTO rapidRatings = liveRatingCacheService.findRapidRatings(0, 100, DEFAULT_SEARCH);
+        TopRatingResponseDTO blitzRatings = liveRatingCacheService.findBlitzRatings(0, 100, DEFAULT_SEARCH);
         TopRatingsResponseDTO response = new TopRatingsResponseDTO(stdRatings, rapidRatings, blitzRatings);
         return ResponseEntity.ok(response);
     }
@@ -33,30 +33,33 @@ public class LiveRatingController {
     public ResponseEntity<TopRatingResponseDTO> getStdRatings(
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "100") int size,
-            @RequestParam(defaultValue = "ALL") String country) {
+            @RequestParam(defaultValue = "ALL") String country,
+            @RequestParam(defaultValue = "") String search) {
         if(!Objects.equals(country, "ALL"))
-            return ResponseEntity.ok(liveRatingCacheService.findStdRatingsByCountry(country, page, size));
-        return ResponseEntity.ok(liveRatingCacheService.findStdRatings(page, size));
+            return ResponseEntity.ok(liveRatingCacheService.findStdRatingsByCountry(country, page, size, search));
+        return ResponseEntity.ok(liveRatingCacheService.findStdRatings(page, size, search));
     }
 
     @GetMapping("/rapid-ratings")
     public ResponseEntity<TopRatingResponseDTO> getRapidRatings(
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "100") int size,
-            @RequestParam(defaultValue = "ALL") String country
+            @RequestParam(defaultValue = "ALL") String country,
+            @RequestParam(defaultValue = "") String search
     ) {
         if(!Objects.equals(country, "ALL"))
-            return ResponseEntity.ok(liveRatingCacheService.findRapidRatingsByCountry(country, page, size));
-        return ResponseEntity.ok(liveRatingCacheService.findRapidRatings(page, size));
+            return ResponseEntity.ok(liveRatingCacheService.findRapidRatingsByCountry(country, page, size, search));
+        return ResponseEntity.ok(liveRatingCacheService.findRapidRatings(page, size, search));
     }
 
     @GetMapping("/blitz-ratings")
     public ResponseEntity<TopRatingResponseDTO> getBlitzRatings(
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "100") int size,
-            @RequestParam(defaultValue = "ALL") String country) {
+            @RequestParam(defaultValue = "ALL") String country,
+            @RequestParam(defaultValue = "") String search) {
         if(!Objects.equals(country, "ALL"))
-            return ResponseEntity.ok(liveRatingCacheService.findBlitzRatingsByCountry(country, page, size));
-        return ResponseEntity.ok(liveRatingCacheService.findBlitzRatings(page, size));
+            return ResponseEntity.ok(liveRatingCacheService.findBlitzRatingsByCountry(country, page, size, search));
+        return ResponseEntity.ok(liveRatingCacheService.findBlitzRatings(page, size, search));
     }
 }
--- src/main/java/com/example/demo/service/LiveRatingCacheService.java
+++ src/main/java/com/example/demo/service/LiveRatingCacheService.java
@@ -58,50 +58,69 @@ public class LiveRatingCacheService {
     }
 
 
-    public TopRatingResponseDTO findStdRatings(int page, int size) {
-        int total = cachedStd.size();
+    public TopRatingResponseDTO findStdRatings(int page, int size, String nameOrId) {
+        return findBySearch(cachedStd, page, size, nameOrId);
+    }
+
+    public TopRatingResponseDTO findRapidRatings(int page, int size, String nameOrId) {
+        return findBySearch(cachedRapid, page, size, nameOrId);
+    }
+
+    public TopRatingResponseDTO findBlitzRatings(int page, int size, String nameOrId) {
+        return findBySearch(cachedBlitz, page, size, nameOrId);
+    }
+
+    public TopRatingResponseDTO findBySearch(List<FullTopRatingDTO> cache, int page, int size, String nameOrId){
+        List<FullTopRatingDTO> content = new ArrayList<>();
+
+        if(nameOrId != null && !nameOrId.isBlank()){
+            int rank = 1;
+            for(FullTopRatingDTO fullTopRatingDTO : cache) {
+                if(isSimilar(nameOrId, fullTopRatingDTO.getName(), String.valueOf(fullTopRatingDTO.getFideId()))){
+                    FullTopRatingDTO copy = new FullTopRatingDTO(fullTopRatingDTO);
+                    copy.setRank((long) rank++);
+                    content.add(copy);
+                }
+            }
+        } else {
+            content = cache;
+        }
+
+        int total = content.size();
         int from = Math.min(page * size, total);
         int to = Math.min(from + size, total);
 
-        return new TopRatingResponseDTO(cachedStd.subList(from, to), total);
+        return new TopRatingResponseDTO(from >= total ? List.of() : content.subList(from, to), content.size());
     }
 
-    public TopRatingResponseDTO findRapidRatings(int page, int size) {
-        int total = cachedRapid.size();
-        int from = Math.min(page * size, total);
-        int to = Math.min(from + size, total);
-
-        return new TopRatingResponseDTO(cachedRapid.subList(from, to), total);
+    public TopRatingResponseDTO findStdRatingsByCountry(String country, int page, int size, String nameOrFideId) {
+        return getRatingsByCountry(cachedStd, country, page, size, nameOrFideId);
     }
 
-    public TopRatingResponseDTO findBlitzRatings(int page, int size) {
-        int total = cachedBlitz.size();
-        int from = Math.min(page * size, total);
-        int to = Math.min(from + size, total);
-
-        return new TopRatingResponseDTO(cachedBlitz.subList(from, to), total);
+    public TopRatingResponseDTO findRapidRatingsByCountry(String country, int page, int size, String nameOrFideId) {
+        return getRatingsByCountry(cachedRapid, country, page, size, nameOrFideId);
     }
 
-    public TopRatingResponseDTO findStdRatingsByCountry(String country, int page, int size) {
-        return getRatingsByCountry(cachedStd, country, page, size);
+    public TopRatingResponseDTO findBlitzRatingsByCountry(String country, int page, int size, String nameOrFideId) {
+        return getRatingsByCountry(cachedBlitz, country, page, size, nameOrFideId);
     }
 
-    public TopRatingResponseDTO findRapidRatingsByCountry(String country, int page, int size) {
-        return getRatingsByCountry(cachedRapid, country, page, size);
-    }
-
-    public TopRatingResponseDTO findBlitzRatingsByCountry(String country, int page, int size) {
-        return getRatingsByCountry(cachedBlitz, country, page, size);
-    }
-
-    private TopRatingResponseDTO getRatingsByCountry(List<FullTopRatingDTO> cache, String country, int page, int size){
+    private TopRatingResponseDTO getRatingsByCountry(List<FullTopRatingDTO> cache, String country, int page, int size, String nameOrFideId){
         List<FullTopRatingDTO> filtered = new ArrayList<>();
         int rank = 1;
         for(FullTopRatingDTO dto : cache){
             if(dto.getCountry().equalsIgnoreCase(country)){
-                FullTopRatingDTO copy = new FullTopRatingDTO(dto);
-                copy.setRank((long) rank++);
-                filtered.add(copy);
+                if(nameOrFideId.isEmpty()){
+                    FullTopRatingDTO copy = new FullTopRatingDTO(dto);
+                    copy.setRank((long) rank++);
+                    filtered.add(copy);
+                } else {
+                    if(isSimilar(nameOrFideId, dto.getName(), String.valueOf(dto.getFideId()))){
+                        FullTopRatingDTO copy = new FullTopRatingDTO(dto);
+                        copy.setRank((long) rank++);
+                        filtered.add(copy);
+                    }
+                }
             }
         }
 
@@ -110,5 +129,20 @@ public class LiveRatingCacheService {
         int to = Math.min(from + size, total);
 
         return new TopRatingResponseDTO(from >= total ? List.of() : filtered.subList(from, to), filtered.size());
+    }
+
+    private boolean isSimilar(String nameOrFideId, String name, String fideID){
+        return formatName(name).toLowerCase().contains(nameOrFideId.toLowerCase()) || fideID.contains(nameOrFideId);
+    }
+
+    private String formatName(String fullName) {
+        String[] parts = fullName.split(",");
+
+        if(parts.length == 2) {
+            return (parts[1].trim() + " " + parts[0].trim())
+                    .toLowerCase();
+        }
+
+        return fullName.trim().toLowerCase();
     }
 }
